e4b561069d4b9faf1cfe7e0211be01422cd75001
Harsha vardhan Balaga <harshabalaga45@gmail.com>
2024-10-22 22:54:57 +0530

    add styles to the university page

--- src/components/CollegeDetails/index.js
+++ src/components/CollegeDetails/index.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import './index.css';
 import dataSet from '../data';
+import Navbar from '../Navbar';
 
 const CollegeDetails = (props) => {
     const [data, setData] = useState(null);
@@ -23,69 +24,77 @@ const CollegeDetails = (props) => {
     const { courses, accommodationDetails } = data;
 
     return (
-        <div>
-            <div>
+        <div className='college-details'>
+            <Navbar />
+            <div className='college-name'>
+                <div className='names'>
                 <h2>{data.collegeName}</h2>
-                <p>Location: {data.location}</p>
-                <p>{data.description}</p>
+                <p className='location'>Location: {data.location}</p>
+                <p className='desc'>{data.description}</p>
+                </div>
+                <div className='image'>
+                    <img src='https://images.apiabroad.com/image/upload/ar_16:9,c_fill,g_auto,w_1600,h_900/v1691176508/programLead/null/32846470376_0346117db5_o_hpmpmx.jpg' />
+                </div>
             </div>
+                <h1 className='heading'>Programs</h1>
             <div className='courses-section'>
+
                 {courses.BTech && (
-                    <div>
+                    <div className='courses'>
                         <h3>B.Tech Programs</h3>
                         {courses.BTech.map((course, index) => (
                             <div key={index} className='course-item'>
-                                <p>Course Name: {course.name}</p>
-                                <p>Fees: {course.fees}</p>
+                                <p>Course Name: <span>{course.name}</span></p>
+                                <p>Fees: <span>{course.fees}</span></p>
                             </div>
                         ))}
                     </div>
                 )}
 
                 {courses.MTech && (
-                    <div>
+                    <div className='courses'>
                         <h3>M.Tech Programs</h3>
                         {courses.MTech.map((course, index) => (
                             <div key={index} className='course-item'>
-                                <p>Course Name: {course.name}</p>
-                                <p>Fees: {course.fees}</p>
+                               <p>Course Name: <span>{course.name}</span></p>
+                               <p>Fees: <span>{course.fees}</span></p>
                             </div>
                         ))}
                     </div>
                 )}
 
                 {courses.PhD && (
-                    <div>
+                    <div className='courses'>
                         <h3>Ph.D. Programs</h3>
                         {courses.PhD.map((course, index) => (
                             <div key={index} className='course-item'>
-                                <p>Course Name: {course.name}</p>
-                                <p>Fees: {course.fees}</p>
+                                <p>Course Name: <span>{course.name}</span></p>
+                                <p>Fees: <span>{course.fees}</span></p>
                             </div>
                         ))}
                     </div>
                 )}
 
                 {courses.MBA && (
-                    <div>
+                    <div className='courses'>
                         <h3>MBA Programs</h3>
                         {courses.MBA.map((course, index) => (
                             <div key={index} className='course-item'>
-                                <p>Course Name: {course.name}</p>
-                                <p>Fees: {course.fees}</p>
+                                <p>Course Name: <span>{course.name}</span></p>
+                                <p>Fees: <span>{course.fees}</span></p>
                             </div>
                         ))}
                     </div>
                 )}
             </div>
+                <h3 className='heading'>Accommodation details</h3>
             <div className='accommodation-section'>
-                <h3>Accommodation Details</h3>
                 {accommodationDetails.map((accommodation) => (
                     <div key={accommodation.id} className='accommodation-item'>
-                        <p>Hostel Name: {accommodation.hostelName}</p>
-                        <p>Type of Room: {accommodation.typeOfRoom}</p>
-                        <p>Area: {accommodation.areaName}</p>
-                        <p>Cost: {accommodation.cost}</p>
+                        <p>Hostel Name: <span>{accommodation.hostelName}</span></p>
+                        <p>Type of Room: <span>{accommodation.typeOfRoom}</span></p>
+                        <p>Area: <span>{accommodation.areaName}</span></p>
+                        <p>Cost: <span>{accommodation.cost}</span></p>
                     </div>
                 ))}
             </div>
